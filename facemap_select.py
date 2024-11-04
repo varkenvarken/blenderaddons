8ec1cc7b55be9472192c0cbef30a714ba910a6ca
TYO79 <54005247+TYO79@users.noreply.github.com>
2024-11-04 17:11:04 +0100

    Update facemap_select.py  with Ui List
    1) Last FaceMap created becomes Active.  change "Facemap" for "FaceMap"
    
    2) Added Ui Lists to Dreate, Delete, Assign, Remove, Select and Deselect FaceMap to Selection
    
    3) Only miss the Up and Down operator

--- facemap_select.py
+++ facemap_select.py
@@ -26,7 +26,7 @@
 
 bl_info = {
     "name": "FacemapSelect",
-    "author": "Michel Anders (varkenvarken) with contribution from Andrew Leichter (aleichter)",
+    "author": "Michel Anders (varkenvarken) with contribution from Andrew Leichter (aleichter) Tyo79",
     "version": (0, 0, 20231230140402),
     "blender": (4, 0, 0),
     "location": "Edit mode 3d-view, Select- -> From facemap | Create facemap",
@@ -36,8 +36,9 @@ bl_info = {
     "category": "Mesh",
 }
 
-
 import bpy
+from bpy.types import Menu, Panel, UIList
+import bmesh
 
 
 class FacemapSelect(bpy.types.Operator):
@@ -51,11 +52,11 @@ class FacemapSelect(bpy.types.Operator):
     @classmethod
     def poll(self, context):
         return (
-            context.mode == "EDIT_MESH"
-            and context.active_object.type == "MESH"
-            and context.active_object.data.attributes.active is not None
-            and context.active_object.data.attributes.active.domain == "FACE"
-            and context.active_object.data.attributes.active.data_type == "BOOLEAN"
+                context.mode == "EDIT_MESH"
+                and context.active_object.type == "MESH"
+                and context.active_object.data.attributes.active is not None
+                and context.active_object.data.attributes.active.domain == "FACE"
+                and context.active_object.data.attributes.active.data_type == "BOOLEAN"
         )
 
     def execute(self, context):
@@ -64,8 +65,8 @@ class FacemapSelect(bpy.types.Operator):
         attribute_name = context.active_object.data.attributes.active.name
         bpy.ops.object.editmode_toggle()
         for polygon, facemap_attribute in zip(
-            context.active_object.data.polygons,
-            context.active_object.data.attributes[attribute_name].data,
+                context.active_object.data.polygons,
+                context.active_object.data.attributes[attribute_name].data,
         ):
             polygon.select |= (
                 facemap_attribute.value
@@ -93,12 +94,154 @@ class FacemapCreate(bpy.types.Operator):
     def execute(self, context):
         bpy.ops.object.editmode_toggle()
         facemap = context.active_object.data.attributes.new(
-            name="Facemap", domain="FACE", type="BOOLEAN"
+            name="FaceMap", domain="FACE", type="BOOLEAN"
         )
         for polygon in context.active_object.data.polygons:
             facemap.data[polygon.index].value = polygon.select
         bpy.ops.object.editmode_toggle()
+
+        attrs = context.object.data.attributes
+        attrs.active = attrs[-1]
+
+
         return {"FINISHED"}
+
+class FacemapAssign(bpy.types.Operator):
+    bl_idname = "mesh.facemap_assign"
+    bl_label = "FacemapAssign"
+
+    param : bpy.props.StringProperty()
+
+    def execute(self, context):
+        obj = context.object
+        facemap = context.object.data.attributes.active
+        attribute_name = facemap.name
+
+        bpy.ops.object.editmode_toggle()
+
+        if attribute_name in obj.data.attributes:
+            attribute = obj.data.attributes[attribute_name]
+            for poly in obj.data.polygons:
+                if poly.select:
+                    if self.param == "Assign":
+                        attribute.data[poly.index].value = True
+                    if self.param == "Remove":
+                        attribute.data[poly.index].value = False
+
+        bpy.ops.object.editmode_toggle()
+
+        return {"FINISHED"}
+
+class FacemapSelections(bpy.types.Operator):
+    bl_idname = "mesh.facemap_selections"
+    bl_label = "Facemap Selections"
+
+    param : bpy.props.StringProperty()
+
+    def execute(self, context):
+        obj = context.object
+
+        facemap = context.object.data.attributes.active
+        attribute_name = facemap.name
+
+        bpy.ops.object.editmode_toggle()
+
+        if attribute_name in obj.data.attributes:
+            attribute = obj.data.attributes[attribute_name]
+            for poly in obj.data.polygons:
+                # if poly.select:
+                if attribute.data[poly.index].value:
+                    if self.param == "Select":
+                        poly.select = True
+                    if self.param == "Deselect":
+                        poly.select = False
+            edge = bpy.context.object.data.edges
+            for i in edge:
+                i.select = False
+
+        bpy.ops.object.editmode_toggle()
+
+        return {"FINISHED"}
+
+
+class FMS_OT_facemap_delete(bpy.types.Operator):
+    bl_idname = "fms.facemap_delete"
+    bl_label = "FacemapDelete"
+    bl_description = (
+        "Delete Active face map "
+    )
+    bl_options = {"REGISTER", "UNDO"}
+
+    def execute(self, context):
+
+        bpy.ops.geometry.attribute_remove()
+        return {"FINISHED"}
+
+
+class MESH_UL_fmaps(UIList):
+
+    def filter_items(self, context, data, propname):
+        items = getattr(data, propname)
+        flt_flags = [self.bitflag_filter_item if item.name.startswith("FaceMap") else 0 for item in items]
+        return flt_flags, []
+
+    def draw_item(self, _context, layout, _data, item, icon, _active_data, _active_propname, _index,):
+
+        fmap = item
+        if self.layout_type in {'DEFAULT', 'COMPACT'}:
+            layout.prop(fmap, "name", text="", emboss=False, icon='FACE_MAPS')
+
+        elif self.layout_type == 'GRID':
+            layout.alignment = 'CENTER'
+            layout.label(text="", icon_value=icon)
+
+
+class DATA_PT_face_maps(Panel):
+    bl_label = "Face Maps"
+    bl_space_type = 'PROPERTIES'
+    bl_context = 'data'
+    bl_region_type = 'WINDOW'
+    bl_options = {'DEFAULT_CLOSED'}
+    COMPAT_ENGINES = {'BLENDER_RENDER', 'BLENDER_EEVEE', 'BLENDER_WORKBENCH', 'BLENDER_WORKBENCH_NEXT'}
+
+    # param: bpy.props.StringProperty()
+
+    @classmethod
+    def poll(cls, context):
+        obj = context.object
+        return (obj and obj.type == 'MESH')
+
+    def draw(self, context):
+        layout = self.layout
+
+        ob = context.object
+        mesh = context.mesh
+        attributes = context.object.data.attributes
+
+        face_maps = [att for att in attributes if att.name.startswith("FaceMap")]
+
+        facemap = mesh.attributes.active
+        rows = 2
+        if facemap:
+            rows = 4
+
+        row = layout.row()
+        row.template_list("MESH_UL_fmaps", "", ob.data, "attributes", ob.data.attributes, "active_index", rows=rows)
+
+        col = row.column(align=True)
+        col.operator("mesh.facemap_create", icon='ADD', text="")
+        col.operator("fms.facemap_delete", icon='REMOVE', text="")
+
+        if len(face_maps)>0 and (ob.mode == 'EDIT' and ob.type == 'MESH'):
+           row = layout.row()
+
+           sub = row.row(align=True)
+           sub.operator("mesh.facemap_assign", text="Assign").param = "Assign"
+           sub.operator("mesh.facemap_assign", text="Remove").param = "Remove"
+
+           sub = row.row(align=True)
+           sub.operator("mesh.facemap_selections", text="Select").param = "Select"
+           sub.operator("mesh.facemap_selections", text="Deselect").param = "Deselect"
 
 
 def menu_func(self, context):
@@ -107,16 +250,33 @@ def menu_func(self, context):
     self.layout.operator(FacemapCreate.bl_idname, text="Create facemap")
 
 
+classes = [
+
+    FacemapSelect,
+    FacemapCreate,
+
+    FMS_OT_facemap_delete,
+    FacemapAssign,
+    FacemapSelections,
+
+    MESH_UL_fmaps,
+    DATA_PT_face_maps,
+
+]
+
+
 def register():
-    bpy.utils.register_class(FacemapSelect)
-    bpy.utils.register_class(FacemapCreate)
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
     bpy.types.VIEW3D_MT_select_edit_mesh.append(menu_func)
 
 
 def unregister():
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
+
     bpy.types.VIEW3D_MT_select_edit_mesh.remove(menu_func)
-    bpy.utils.unregister_class(FacemapSelect)
-    bpy.utils.unregister_class(FacemapCreate)
 
 
 if __name__ == "__main__":
