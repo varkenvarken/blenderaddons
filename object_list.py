6cb4903c219f569ba46179a2496006b9e4851ad3
varkenvarken <andersmichel8@gmail.com>
2025-09-24 14:34:28 +0200

    Use a DictWriter class instead of handcoding

--- object_list.py
+++ object_list.py
@@ -1,65 +1,81 @@
 #  ##### BEGIN GPL LICENSE BLOCK #####
-#  
+#
 #  ObjectList, a Blender add-on
 #  (c) 2021,2025 Michel Anders (varkenvarken)
-#  
+#
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
-#  
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#  
+#
 #  ##### END GPL LICENSE BLOCK #####
 
 
 bl_info = {
     "name": "ObjectList",
     "author": "Michel Anders (varkenvarken)",
-    "version": (0, 0, 20250924140226),
+    "version": (0, 0, 20250924141758),
     "blender": (4, 4, 0),
     "location": "View3D > Object > Object List",
     "description": "create a comma separated list with object mesh info",
     "warning": "",
     "wiki_url": "",
     "tracker_url": "",
-    "category": "Object"}
+    "category": "Object",
+}
+
+from csv import DictWriter
 
 import bpy
 import bmesh
 
-def object_list(context):
 
+def object_list(context):
     bm = bmesh.new()
 
     objects = context.scene.objects[:]
-    
-    selection_status = {ob:ob.select_get() for ob in objects}
+
+    selection_status = {ob: ob.select_get() for ob in objects}
     active_object = context.active_object
 
-    bpy.ops.object.select_all(action='DESELECT')
+    bpy.ops.object.select_all(action="DESELECT")
 
     for ob in objects:
         ob.select_set(True)
-        bpy.ops.object.convert(target='MESH', keep_original=True)
+        bpy.ops.object.convert(target="MESH", keep_original=True)
 
         for ob2 in context.scene.objects:
             if ob2 not in objects:
-
-                tris, faces, edges, verts = 0,0,0,0
+                tris, faces, edges, verts = 0, 0, 0, 0
                 bm.from_mesh(ob2.data)
                 tris = len(bm.calc_loop_triangles())
                 verts = len(bm.verts)
                 edges = len(bm.edges)
                 faces = len(bm.faces)
 
-                yield ob.name[:], ob.type[:], tris, faces, edges, verts, ob.data.name[:] if ob.data else None, ob.data.users if ob.data else 1, ",".join(c.name for c in ob.users_collection)
+                info = {
+                    "Name": ob.name[:],
+                    "Type": ob.type[:],
+                    "Tris": tris,
+                    "Faces": faces,
+                    "Edges": edges,
+                    "Verts": verts,
+                    "Datablock name": ob.data.name[:] if ob.data else None,
+                    "Users": ob.data.users,
+                }
+                for i,c in enumerate(ob.users_collection, start=1):
+                    info[f"Collection {i}"] = c.name[:]
+
+                yield info
+
                 bm.clear()
 
                 data = ob2.data
@@ -67,34 +83,53 @@ def object_list(context):
                 bpy.data.meshes.remove(data)
 
     for ob in objects:
-        ob.select_set(selection_status[ob])            
+        ob.select_set(selection_status[ob])
     bpy.context.view_layer.objects.active = active_object
 
     bm.free()
 
+
 class ObjectList(bpy.types.Operator):
     bl_idname = "object.object_list"
     bl_label = "ObjectList"
-    bl_options = {'REGISTER', 'UNDO'}
+    bl_options = {"REGISTER", "UNDO"}
 
     def execute(self, context):
         oblist = list(object_list(bpy.context))
-    
+
         t = bpy.data.texts.new(name="Object list.csv")
-        t.write(",".join(("Name","Type","Tris", "Faces", "Edges", "Verts", "Datablock name","Users", "Collection 1","Collection 2","Collection 3")))
-        t.write("\n")
-        for ob in oblist:
-            t.write(",".join(map(str,ob)))
-            t.write("\n")
-        return {'FINISHED'}
+        dictwriter = DictWriter(
+            t,
+            fieldnames=[
+                "Name",
+                "Type",
+                "Tris",
+                "Faces",
+                "Edges",
+                "Verts",
+                "Datablock name",
+                "Users",
+                "Collection 1",
+                "Collection 2",
+                "Collection 3",
+            ],
+        )
+        dictwriter.writeheader()
+        dictwriter.writerows(oblist)
+
+        return {"FINISHED"}
 
 
 def menu_func(self, context):
-    self.layout.operator(ObjectList.bl_idname, text="Create a comma separated list with object info",
-                        icon='INFO')
+    self.layout.operator(
+        ObjectList.bl_idname,
+        text="Create a comma separated list with object info",
+        icon="INFO",
+    )
 
 
-classes = (ObjectList, )
+classes = (ObjectList,)
+
 
 def register():
     for c in classes:
